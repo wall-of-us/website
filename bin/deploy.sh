3800e37673949788ade56b2e70b13927288f0b3e
Dan Aronson <dan@fandor.com>
2017-05-31 17:51:59 -0700

    first pass at environment population

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,14 +7,14 @@ current_branch=`git rev-parse HEAD`
 zip_file_name="${current_branch}.zip"
 command="git archive -o /tmp/$zip_file_name HEAD"
 echo "Zipping archive"
-#eval $command
+eval $command
 
 # copy to s3
 s3_file_path=`date "+ebs-apps/%Y/%m/%d/${zip_file_name}"`
-s3_cp_command="aws s3 cp /tmp/${zip_file_name} s3://wou-storage-bucket/ebs-apps/${s3_file_path}"
+s3_cp_command="aws s3 cp /tmp/${zip_file_name} s3://wou-storage-bucket/${s3_file_path}"
 
 echo "copying to s3://wou-storage-bucket/${s3_file_path}"
-#eval ${s3_cp_command}
+eval ${s3_cp_command}
 
 # send to sns topic to publish
 last_log_message=`git reflog -1 | sed 's/^.*: //'`
@@ -22,4 +22,4 @@ message="${s3_file_path}:${current_branch}:${last_log_message}"
 arn="arn:aws:sns:us-west-2:745087045597:stagingupdate"
 sns_command="aws sns publish --topic-arn \"${arn}\" --message \"${message}\""
 echo "sending to sns, command: '${sns_command}'"
-#eval ${sns_command}
+eval ${sns_command}
