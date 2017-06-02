41c8dd79f0aa2e30568c139bafc6381d97747c67
Dan Aronson <dan@fandor.com>
2017-06-02 00:48:16 -0700

    fixed forced rewrite of ssl (moved into laravel)

--- app/Http/Middleware/ForceSSL.php
+++ app/Http/Middleware/ForceSSL.php
@@ -15,7 +15,8 @@ class ForceSSL
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure()) {
+        /* we are behind a load balancer, so use it's proto, allow health requests to come through unmolested */
+        if(($request->header('x-forwarded-proto') <> 'https') and ('/health' <> $request->getRequestUri())){
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
