0b12f97bd01a0d9462b60d62bae72481ab84b141
Nicole Neditch <nicole@codeforamerica.org>
2017-08-13 12:12:48 -0700

    Updated call file to include all reps

--- resources/views/actions/call.blade.php
+++ resources/views/actions/call.blade.php
@@ -152,14 +152,157 @@ You must <a href="/login">login</a> to see the contact info and scripts for your
       </div>
 
   </td>
-  @endif
-  @endif
+  
 </tr>
-
-
 
 </tbody>
 </table>
+@endif
+@endif
+
+
+<p>If you have updated information email us at <a href="contact@wall-of-us.org">contact@wall-of-us.org</a>.</p>
+@else
+You must <a href="/login">login</a> to see the contact info and scripts for your Representitives.
+@endif
+@endif
+
+<!-- Combined Reps -->
+
+@if ($post->call == 'Reps')
+@if (Auth::check())
+
+<table class="table table-striped">
+<thead>
+<tr>
+  <th>Your Representitive(s)</th>
+  <th>Phone Number</th>
+  @if (isset($post->script))
+  <th>Call Script</th>
+  @endif
+</tr>
+</thead>
+<tbody>
+<tr>
+  @if (isset($senator_1->first_name))
+  <td><a href="/reps/{{ $senator_1->slug }}">Senator {{ $senator_1->first_name }} {{ $senator_1->middle_name }} {{ $senator_1->last_name }}</a></td>
+  <td>{{ $senator_1->phone }}</td>
+  @if (isset($post->script))
+  <td>
+  <!-- Trigger the modal with a button -->
+      <button type="button" class="btn btn-body" data-toggle="modal" data-target="#myModal">Call Script</button>
+
+      <!-- Modal -->
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Senator {{ $senator_1->first_name }} {{ $senator_1->middle_name }} {{ $senator_1->last_name }} -- Call {{ $senator_1->phone }}</h4>
+            </div>
+            <div class="modal-body">
+              <p>
+             {!! str_replace(array('<name>'), array($senator_1->last_name), $post->script) !!}
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+  </td>
+  @endif
+
+</tr>
+
+
+<tr>
+  <td>@if (isset($senator_2->first_name)) <a href="/reps/{{ $senator_2->slug }}">Senator {{ $senator_2->first_name }} {{ $senator_2->middle_name }} {{ $senator_2->last_name }}</a> @endif</td>
+  <td>{{ $senator_1->phone }}</td>
+  @if (isset($post->script))
+  <td>
+
+  <!-- Trigger the modal with a button -->
+      <button type="button" class="btn btn-body" data-toggle="modal" data-target="#myModal2">Call Script</button>
+
+      <!-- Modal -->
+      <div id="myModal2" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Senator {{ $senator_2->first_name }} {{ $senator_2->middle_name }} {{ $senator_2->last_name }} -- Call {{ $senator_2->phone }}</h4>
+            </div>
+            <div class="modal-body">
+            <p>
+              {!! str_replace(array('<name>'), array($senator_2->last_name), $post->script) !!}
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+  </td>
+  @endif
+  @else 
+  <td colspan="4">
+  You have no senators where you live. <a href="/reps">See all senators ></a>
+  </td>
+  @endif
+</tr>
+<tr>
+  @if (isset($rep))
+  <td><a href="/house/{{ $rep_slug }}">Representitive {{ $rep }}</a></td>
+  <td>{{ $rep_phone }}</td>
+  @if (isset($post->script))
+  <td>
+  <!-- Trigger the modal with a button -->
+      <button type="button" class="btn btn-body" data-toggle="modal" data-target="#myModal">Call Script</button>
+
+      <!-- Modal -->
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Representitive {{ $rep }} -- Call {{ $rep_phone }}</h4>
+            </div>
+            <div class="modal-body">
+              <p>
+             {!! str_replace(array('<name>'), array($rep), $post->script) !!}
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+  </td>
+  
+</tr>
+
+</tbody>
+</table>
+@endif
+@endif
+
+
 <p>If you have updated information email us at <a href="contact@wall-of-us.org">contact@wall-of-us.org</a>.</p>
 @else
 You must <a href="/login">login</a> to see the contact info and scripts for your Representitives.
